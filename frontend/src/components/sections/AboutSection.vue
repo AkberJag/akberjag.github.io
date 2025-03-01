7d37f2d3370eaac583e3556dcf5bfa8be127008e
Akber Jagannath <akberjag@gmail.com>
2025-03-01 13:50:38 +0000

    🔀 merge: Merge remote-tracking branch 'template/main'

--- frontend/src/components/sections/AboutSection.vue
+++ frontend/src/components/sections/AboutSection.vue
@@ -1,33 +1,35 @@
 <template>
+  <div class="flex justify-center items-center min-h-screen w-full p-4">
     <div class="max-w-2xl text-center px-4">
-        <h2 class="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white mb-4 md:mb-6">
-            About Me
-        </h2>
-        <div class="text-sm md:text-lg text-gray-600 dark:text-gray-300 space-y-6">
-            <p>
-                A Python enthusiast who loves bringing ideas to life with code. I've got experience working with
-                <span class="font-bold"> Flask </span> and
-                <span class="font-bold"> FastAPI </span> to build cool web apps and APIs, and I'm also pretty handy with
-                <span class="font-bold"> Vue.js </span> to create smooth and interactive front-ends.
-            </p>
+      <h2 class="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white mb-4 md:mb-6">
+        About Me
+      </h2>
+      <div class="text-sm md:text-lg text-gray-600 dark:text-gray-300 space-y-6">
+        <p>
+          A Python enthusiast who loves bringing ideas to life with code. I've got experience working with
+          <span class="font-bold"> Flask </span> and
+          <span class="font-bold"> FastAPI </span> to build cool web apps and APIs, and I'm also pretty handy with
+          <span class="font-bold"> Vue.js </span> to create smooth and interactive front-ends.
+        </p>
 
-            <p>
-                When I'm not coding for work, you'll probably find me making mobile games with
-                <span class="font-bold"> Godot </span>
-                🎮. I love blending creativity with programming, and it's a fun way to keep learning and trying new
-                things.
-            </p>
+        <p>
+          When I'm not coding for work, you'll probably find me making mobile games with
+          <span class="font-bold"> Godot </span>
+          🎮. I love blending creativity with programming, and it's a fun way to keep learning and trying new
+          things.
+        </p>
 
-            <p>
-                Oh, and I'm also a certified <span class="font-bold">amateur radio operator</span> 📡!
-                With some <span class="font-bold">field and rescue</span> experience under my belt,
-                I've gained a knack for <span class="font-bold">problem-solving</span> and working well under pressure.
-            </p>
+        <p>
+          Oh, and I'm also a certified <span class="font-bold">amateur radio operator</span> 📡!
+          With some <span class="font-bold">field and rescue</span> experience under my belt,
+          I've gained a knack for <span class="font-bold">problem-solving</span> and working well under pressure.
+        </p>
 
-            <p>
-                Whether it's developing software, testing games, or experimenting with new projects,
-                I'm always up for a challenge. Let's create something awesome together! 🚀
-            </p>
-        </div>
+        <p>
+          Whether it's developing software, testing games, or experimenting with new projects,
+          I'm always up for a challenge. Let's create something awesome together! 🚀
+        </p>
+      </div>
     </div>
+  </div>
 </template>
